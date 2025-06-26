4d2a12a3484558d7df4eae464cb59240e9790105
Hardik <hardikchhabra3007@gmail.com>
2025-06-26 13:05:01 +0530

    Set debug off

--- billwise/settings.py
+++ billwise/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-your-secret-key-here'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['billwise-0hif.onrender.com', "127.0.0.1"]
 
